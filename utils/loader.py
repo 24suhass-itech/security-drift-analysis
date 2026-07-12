a18034ba5dcc2de27a534a0ccfc67f612de3c63c
24suhass-itech <24suhass@gmail.com>
2026-07-12 15:25:13 +0530

    Remove virtual environment and update .gitignore

--- utils/loader.py
+++ utils/loader.py
@@ -1,5 +1,5 @@
-import json
 import pandas as pd
+import json
 
 def load_baseline():
     with open("backend/baseline_configs.json", "r") as f:
@@ -16,53 +16,52 @@ def load_control_metadata():
 def load_drift_report():
 
     baseline = load_baseline()
-
     events = load_change_events()
-
     metadata = load_control_metadata()
 
-    with open("backend/drift_report.json", "r") as f:
-        drift = pd.DataFrame(json.load(f))
+    drift = pd.read_csv("backend/drift_report.csv")
 
-    df = (
-        drift
-        .merge(
-            baseline[
-                [
-                    "control_id",
-                    "domain",
-                    "system",
-                    "severity_if_drifted"
-                ]
-            ],
-            on="control_id",
-            how="left"
-        )
-        .merge(
-            metadata[
-                [
-                    "control_id",
-                    "business_criticality",
-                    "control_owner",
-                    "asset_type"
-                ]
-            ],
-            on="control_id",
-            how="left"
-        )
-        .merge(
-            events[
-                [
-                    "event_id",
-                    "changed_by",
-                    "change_source",
-                    "approval_status",
-                    "maintenance_window"
-                ]
-            ],
-            on="event_id",
-            how="left"
-        )
+    df = drift.merge(
+        baseline[
+            [
+                "control_id",
+                "domain",
+                "system",
+                "severity_if_drifted"
+            ]
+        ],
+        on="control_id",
+        how="left"
     )
+
+    df = df.merge(
+        metadata[
+            [
+                "control_id",
+                "business_criticality",
+                "control_owner",
+                "asset_type"
+            ]
+        ],
+        on="control_id",
+        how="left"
+    )
+
+    df = df.merge(
+        events[
+            [
+                "event_id",
+                "changed_by",
+                "change_source",
+                "approval_status",
+                "maintenance_window"
+            ]
+        ],
+        on="event_id",
+        how="left"
+    )
+
+    if "severity_if_drifted" in df.columns and "severity" not in df.columns:
+        df["severity"] = df["severity_if_drifted"]
 
     return df
